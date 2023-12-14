9e80d2ef0ec8faeef756b814db9fcb8f942c585a
Gerson Arce <arceger@hotmail.com>
2023-12-14 16:47:35 +0000

    newPageInit

--- src/test/java/com/javaweb/test/UserRepositoryTestes.java
+++ src/test/java/com/javaweb/test/UserRepositoryTestes.java
@@ -1,6 +1,7 @@
 package com.javaweb.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
