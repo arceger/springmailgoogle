46d75c44c039089aad1e597b2167fd96f49c9f0c
Gerson Arce <arceger@hotmail.com>
2023-12-28 20:46:13 +0000

    cors10

--- src/main/java/com/javaweb/WebSecurity.java
+++ src/main/java/com/javaweb/WebSecurity.java
@@ -25,7 +25,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOrigin("*");
+		configuration.addAllowedOrigin("https://mobile-ount.onrender.com");
 		configuration.addAllowedMethod("GET");
 		configuration.addAllowedMethod("PUT");
 		configuration.addAllowedHeader("*");
