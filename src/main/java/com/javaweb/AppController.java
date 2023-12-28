3fefe90e350c78866d8c8a28860135729c0924d0
Gerson Arce <arceger@hotmail.com>
2023-12-28 14:51:29 +0000

    noCors

--- src/main/java/com/javaweb/AppController.java
+++ src/main/java/com/javaweb/AppController.java
@@ -213,12 +213,13 @@ public class AppController {
 			List<User> listUsers = repo.findAll();
 			return listUsers;
 		}
-
+		@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT})
 		@GetMapping("/orderlist") // listar todos os incidentes
 		public List<Orders> viewOrderList() {
 			List<Orders> orderapi = repoOrder.findAll();
 			return orderapi;
 		}
+		@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT})
 		@GetMapping("/listcal/{orderId}") //listar apenas um incidente pelo id
 		@ResponseBody
 		public ResponseEntity<List<Orders>> viewOrderCall(@PathVariable long orderId) {
