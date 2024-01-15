304d928f63e99576369ae39e7a4677176aa295b9
Gerson Arce <arceger@hotmail.com>
2024-01-15 14:18:39 +0000

    newrote

--- src/main/java/com/javaweb/WebSecurity.java
+++ src/main/java/com/javaweb/WebSecurity.java
@@ -26,7 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		//configuration.addAllowedOrigin("https://mobile-ount.onrender.com");
+		configuration.addAllowedOrigin("https://overdatalab.000webhostapp.com");
 		configuration.addAllowedOrigin("https://mobi-ldvv.onrender.com");
 		configuration.addAllowedMethod("GET");
 		configuration.addAllowedMethod("POST");
